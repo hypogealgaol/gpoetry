874cca25cd612b55122ba3c6b7e5b8f615e2e818
Sinjihn Smith <sinjihnsmith@gmail.com>
2015-06-16 20:14:42 -0400

    (files)added webapp files

---
+++ appscript.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+
+	$( '#button' ).click(function() {
+	  alert( "Submitted" );
+	});
+
+
+
+	$('#button').click(function() {
+		var textInp = document.getElementById('inputT').value;
+		console.log(textInp); 
+
+	}); 
+
+	//google search thist textInpu
+	
+
+
+})
+
+
